d738b0e717e1baf8790dc7e7e967b97a968eedd1
FranG21 <45045305+FranG21@users.noreply.github.com>
2022-06-17 22:16:39 -0600

    commit 2

---
+++ src/db/migrations/20220618041232_alter_table_user.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+    return knex.schema.alterTable('user', (table) => {
+        table.dropColumn('customer_id');
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+    return knex.schema.alterTable('user', (table) => {
+        table.integer('customer_id').references('id').inTable('customer');
+    });
+};
